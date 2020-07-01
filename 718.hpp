0d6720778ef1257f044a1d1717c68476002142e2
fun4algs <66786481+fun4algs@users.noreply.github.com>
2020-07-02 00:27:47 +0800

    Update 718.hpp

--- 718.hpp
+++ 718.hpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int findLength(vector<int>& A, vector<int>& B) {
         int m = A.size(), n = B.size();
+        int dp[m + 1][n + 1];
+        memset(dp, 0, sizeof dp);
         int res = 0;
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                dp[i][j] = (A[i - 1] == B[j - 1]) ? dp[i - 1][j - 1] + 1 : 0;
+        for(int i = 1; i <= m; ++i){
+            for(int j = 1; j <= n; ++j){
+                dp[i][j] = A[i -1] == B[j - 1] ? dp[i - 1][j - 1] + 1 : 0;
                 res = max(res, dp[i][j]);
             }
         }
