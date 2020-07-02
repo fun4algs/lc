ef6b43666bba0a2e9497cf8a59d75050a61cf8d7
fun4algs <algo@bupt.edu.cn>
2020-07-02 21:32:25 +0800

    Jul 2 update

--- 378.hpp
+++ 378.hpp
@@ -1,3 +1,4 @@
+//time complexity:O(nlogn*logM)
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& m, int k) {
@@ -15,3 +16,30 @@ public:
         return l;
     }
 };
+
+//time complexity:O(nlogM)
+int kthSmallest(vector<vector<int>>&m, int k){
+    int l = m[0][0], r = m.back().back();
+    int n = m.size();
+    while(l < r){
+        int mid = l + (r - l) / 2;
+        int cnt = helper(m, mid);
+        if(cnt < k) l = mid + 1;
+        else r = mid;
+    }
+    return l;
+}
+
+int helper(vector<vector<int>>&m, int tar){
+    int n = m.size();
+    int i = n - 1, j = 0;
+    int cnt = 0;
+    while(i >= 0 && j < n){
+        if(m[i][j] <= tar){
+            cnt += i + 1;
+            ++j;
+        }
+        else --i;
+    }
+    return cnt;
+}
