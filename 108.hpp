ae05a2212fb7682b23f986086c2b60809691c5de
fun4algs <algo@bupt.edu.cn>
2020-07-03 11:06:08 +0800

    Jul 3

---
+++ 108.hpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+    TreeNode* helper(vector<int>&nums, int l, int r){
+        if(l < r) return nullptr;
+        int mid = l + (r - l) / 2;
+        TreeNode* p = new TreeNode(nums[mid]);
+
+        p->left = helper(nums, l, mid - 1);
+        p->right = helper(nums, mid + 1, r);
+        return p;
+    }
+};
